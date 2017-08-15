d57b88e66e45caeebbe3aff942b46ead7513882d
hca <hca@itam.mx>
2017-08-15 17:18:37 -0500

    Added basic concepts

---
+++ Intro/Conceptos_basicos.m
@@ -0,0 +1,39 @@
+% Los comentarios se ponen con la tecla "%".
+
+% Declarar e inicilizar una variable.
+a = 4.51;
+
+% Llamar la funcion seno.
+sin(a);
+
+% Si queremos que la consola vaya imprimiendo lo que se hizo, entonces no
+% poner el ";" al final del enunciado.
+exp(a)
+
+% Calculando raiz cuadrada. 
+b = sqrt(a);
+
+% Imprimiendo a la consola en formato long.
+format long
+b
+
+% Imprimiendo a la consola en formato corto.
+format short
+b
+
+% Jugando.
+format long
+
+% No lo alcanza a ver.
+n = 10^16;
+1 + 1/n
+
+% Si lo alcanza a ver.
+n = 10^15; 
+1 + 1/n
+
+% Not a Number result.
+0/0
+
+% Infinity is 
+1/Inf
